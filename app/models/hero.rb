c809fcb7d12c02b7210fa0bfd36f2e8464520efa
Alex Velasquez <jtinnesco@gmail.com>
2020-02-19 19:49:43 +0000

    Automatically backed up by Learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -2,7 +2,7 @@ class Hero
   attr_accessor :name, :power, :bio
 
   def initialize(input)
-    @name = input[:name]
+    @name  = input[:name]
     @power = input[:power]
     @bio   = input[:bio]
 
