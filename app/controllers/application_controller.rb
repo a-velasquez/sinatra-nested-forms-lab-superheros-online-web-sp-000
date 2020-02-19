8b349a66beb056d587d9c1a8ba3a601794bf2729
Alex Velasquez <jtinnesco@gmail.com>
2020-02-19 19:52:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,12 +9,14 @@ class App < Sinatra::Base
     end
 
     post '/teams' do
-      @team_name = params["team"]["name"]
-      @team_motto = params["team"]["motto"]
-      @team_members = params["team"]["members"]
+      @team = Team.new(params[:team])
 
-      erb :'../views/team'
+      params[:team][:heroes].each do |args|
+        Hero.new(args)
+      end
+
+      @heroes = Hero.all
+
+      erb :team
     end
-
-
 end
